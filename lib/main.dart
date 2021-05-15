ef3d9770fc87fae5eb898c3dc0620186f4e299ff
JyotheendraD <75023743+JyotheendraD@users.noreply.github.com>
2021-05-15 09:46:55 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.white,
-              ),
-              Text(
-                'Jyotheendra',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-import 'package:flutter/material.dart';
-
 void main()
 {
   runApp(MyApp());
@@ -136,15 +91,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
